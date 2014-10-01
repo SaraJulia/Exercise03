e5e16254a5b161bd6eee1087947263941fbaa656
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:32:00 -0700

    fixed import arithmetic

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-import arithmetic.py
+import arithmetic
 
 while True:
     input = raw_input("--> ")
@@ -6,20 +6,20 @@ while True:
     if tokens[0] == "q":
         quit()
     elif tokens[0] == "+":
-        print add(float(tokens[1]),float(tokens[2]))
+        print arithmetic.add(float(tokens[1]),float(tokens[2]))
     elif tokens[0] == "-":
-        print subtract(float(tokens[1]),float(tokens[2]))
+        print arithmetic.subtract(float(tokens[1]),float(tokens[2]))
     elif tokens[0] == "*":
-        print multiply(float(tokens[1]),float(tokens[2]))
+        print arithmetic.multiply(float(tokens[1]),float(tokens[2]))
     elif tokens[0] == "/":
-        print divide(float(tokens[1]),float(tokens[2]))
+        print arithmetic.divide(float(tokens[1]),float(tokens[2]))
     elif tokens[0] == "square":
-        print square(float(tokens[1]))
+        print arithmetic.square(float(tokens[1]))
     elif tokens[0] == "cube":
-        print cube(float(tokens[1]))
+        print arithmetic.cube(float(tokens[1]))
     elif tokens[0] == "pow":
-        print power(float(tokens[1]),float(tokens[2]))
+        print arithmetic.power(float(tokens[1]),float(tokens[2]))
     elif tokens[0] == "mod":
-        print mod(float(tokens[1]),float(tokens[2]))
+        print arithmetic.mod(float(tokens[1]),float(tokens[2]))
     else:
         print "enter a valid expression"
