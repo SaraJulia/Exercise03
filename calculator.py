3c4ea64e929d3d17816d80e46d1c83b86835124e
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:45:18 -0700

    created first, second variables to clean up code

--- calculator.py
+++ calculator.py
@@ -1,25 +1,31 @@
 import arithmetic
 
-while True:
-    input = raw_input("--> ")
-    tokens = input.split(" ")
-    if tokens[0] == "q":
-        quit()
-    elif tokens[0] == "+":
-        print arithmetic.add(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "-":
-        print arithmetic.subtract(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "*":
-        print arithmetic.multiply(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "/":
-        print arithmetic.divide(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "square":
-        print arithmetic.square(float(tokens[1]))
-    elif tokens[0] == "cube":
-        print arithmetic.cube(float(tokens[1]))
-    elif tokens[0] == "pow":
-        print arithmetic.power(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "mod":
-        print arithmetic.mod(float(tokens[1]),float(tokens[2]))
-    else:
-        print "enter a valid expression"
+def main():
+    while True:
+        input = raw_input("--> ")
+        tokens = input.split()
+        if tokens[0] == "q":
+            quit()
+        first = float(tokens[1])
+        second = float(tokens[2])
+        if tokens[0] == "+":
+            print arithmetic.add(first,second)
+        elif tokens[0] == "-":
+            print arithmetic.subtract(first,second)
+        elif tokens[0] == "*":
+            print arithmetic.multiply(first,second)
+        elif tokens[0] == "/":
+            print arithmetic.divide(first,second)
+        elif tokens[0] == "square":
+            print arithmetic.square(first)
+        elif tokens[0] == "cube":
+            print arithmetic.cube(first)
+        elif tokens[0] == "pow":
+            print arithmetic.power(first,second)
+        elif tokens[0] == "mod":
+            print arithmetic.mod(first,second)
+        else:
+            print "enter a valid expression"
+
+if __name__ == "__main__":
+    main()
