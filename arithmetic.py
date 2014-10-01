dfa018bc7a275fd2c72ade194dd482133856ec5a
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 16:10:20 -0700

    changed addition and subtraction to handle >2 arguments

--- arithmetic.py
+++ arithmetic.py
@@ -1,8 +1,18 @@
-def add(num1, num2):
-    return num1 + num2
+def add(nums):
+    thesum = 0
+    i = 0
+    while i < len(nums):
+        thesum = thesum + nums[i]
+        i += 1
+    return thesum
 
-def subtract(num1, num2):
-    return num1 - num2
+def subtract(nums):
+    thediff = nums[0] - nums[1]
+    i = 2
+    while i < len(nums):
+        thediff = thediff - nums[i]
+        i += 1
+    return thediff
 
 def multiply(num1, num2):
     return num1 * num2
--- calculator.py
+++ calculator.py
@@ -6,24 +6,29 @@ def main():
         tokens = input.split()
         if tokens[0] == "q":
             quit()
-        first = float(tokens[1])
-        second = float(tokens[2])
+
+        listofnums = []
+        i = 1
+        while i < len(tokens):
+            listofnums = listofnums + [float(tokens[i])]
+            i += 1
+
         if tokens[0] == "+":
-            print arithmetic.add(first,second)
+            print arithmetic.add(listofnums)
         elif tokens[0] == "-":
-            print arithmetic.subtract(first,second)
+            print arithmetic.subtract(listofnums)
         elif tokens[0] == "*":
-            print arithmetic.multiply(first,second)
+            print arithmetic.multiply(listofnums)
         elif tokens[0] == "/":
-            print arithmetic.divide(first,second)
+            print arithmetic.divide(listofnums)
         elif tokens[0] == "square":
-            print arithmetic.square(first)
+            print arithmetic.square(tokens[1])
         elif tokens[0] == "cube":
-            print arithmetic.cube(first)
+            print arithmetic.cube(tokens[1])
         elif tokens[0] == "pow":
-            print arithmetic.power(first,second)
+            print arithmetic.power(listofnums)
         elif tokens[0] == "mod":
-            print arithmetic.mod(first,second)
+            print artnthmetic.mod(listofnums)
         else:
             print "enter a valid expression"
 
