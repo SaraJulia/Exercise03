f82bda289a451cec61a23626de3f0963054f599e
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:29:38 -0700

    deleted while loop from arithmetic file

--- arithmetic.py
+++ arithmetic.py
@@ -21,27 +21,3 @@ def power(num1, num2):
 
 def mod(num1, num2):
     return num1 % num2
-
-while True:
-    input = raw_input("--> ")
-    tokens = input.split(" ")
-    if tokens[0] == "q":
-        quit()
-    elif tokens[0] == "+":
-        print add(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "-":
-        print subtract(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "*":
-        print multiply(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "/":
-        print divide(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "square":
-        print square(float(tokens[1]))
-    elif tokens[0] == "cube":
-        print cube(float(tokens[1]))
-    elif tokens[0] == "pow":
-        print power(float(tokens[1]),float(tokens[2]))
-    elif tokens[0] == "mod":
-        print mod(float(tokens[1]),float(tokens[2]))
-    else:
-        print "enter a valid expression"
